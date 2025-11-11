16bbcead13c7d38961840fc4d25f12194930ad49
Asus <murthyk011@gmail.com>
2025-11-11 17:34:12 +0530

    lines deleted

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import model.Subject;
 import service.StudentServiceImpl;
 import model.Student;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -31,7 +30,7 @@ public class Main {
             System.out.println("6: Get student by group");
 
 
-            System.out.println("6: Ex");
+
             System.out.println("6: Exit");
 
             choice = sc.nextInt();
